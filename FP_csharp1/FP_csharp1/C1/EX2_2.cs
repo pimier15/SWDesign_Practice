a4621ed57c4aa7c4f1a3abedcb29ef8c4a74cb51
pimier15 <pimier15@gmail.com>
2017-11-03 17:13:14 +0900

    First Commit

---
+++ FP_csharp1/FP_csharp1/C1/EX2_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP_csharp1.C1
+{
+	public static class EX2_2
+	{
+		public static void main()
+		{
+
+
+		}
+
+	}
+
+	public static class ListFormatter
+	{
+		
+	}
+
+
+}
---
+++ FP_csharp1/FP_csharp1/C1/Ex2_2 Avoid Mutating.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP_csharp1.C1
+{
+	class Ex1
+	{
+		decimal RecomputeTotal( Order order , List<OrderLine> linesToDelete )
+		{
+			var result = 0m;
+			foreach ( var line in order.OrderLines)
+			{
+
+			}
+		}
+
+		class Product
+		{
+			public decimal Price { get; }
+		}
+
+		class OrderLine
+		{
+			public Product Product { get; }
+			public int Quantity { get; }
+		}
+
+		class Order
+		{
+			public IEnumerable<OrderLine> OrderLines { get; }
+		}
+	}
+}
