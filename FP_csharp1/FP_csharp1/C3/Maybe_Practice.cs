9d0f5261c7180a3bc15455faafee140e790fb0d0
pimier15 <pimier15@gmail.com>
2017-11-04 21:31:16 +0900

    update Maybe and Implement

---
+++ FP_csharp1/FP_csharp1/C3/Maybe_Practice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+namespace FP_csharp1.C3
+{
+	class Maybe_Practice
+	{
+		
+		Option<string> _ = None;
+		Option<string> john = Some("John");
+
+		string greet( Option<string> greetee )
+			=> greetee.Match(
+				None : () => "Sorry" ,
+				Some : (name) => "Hi");
+
+		public void main()
+		{
+
+		}
+	}
+
+
+
+}
---
+++ FP_csharp1/Implement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Implement.Lib.F;
+
+namespace Implement
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+
+			var kk = None;
+
+		}
+	}
+}
