6e76a20ddfe77e3aa84bc952a018d1f7944b5bbf
pimier15 <pimier15@gmail.com>
2017-11-09 08:53:56 +0900

    update

--- FP_csharp1/Test/Program.cs
+++ FP_csharp1/Test/Program.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.immitable
+using System.Collections.Immutable;
 
 namespace Test
 {
+	using static ext;
 	class Program
 	{
 		static void Main( string[] args )
 		{
+
+			var tt = List(2,3,4,5,6,7,5,3);
+
 		}
 	}
 
 	public static class ext
 	{
+		public static IEnumerable<T> List<T>( params T[] items )
+			=> items.ToImmutableList().ToArray();
 
 
 	}
