7871c94ef2a0d129aae1fcbda8746ecd2563706a
pimier15 <pimier15@gmail.com>
2017-11-11 08:44:12 +0900

    update

---
+++ FP_csharp1/ExampleBasedLib/Datas/Type.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleBasedLib.Datas
+{
+	public static class Type
+	{
+		public static void main()
+		{
+			var temp = new Age(2);
+			var addAge = temp + 10;
+			var res = temp*10;
+
+
+		}
+
+	}
+
+	public class Age
+	{
+		public readonly double age;
+		public Age( double age )
+		{ this.age = age; }
+
+		public static implicit operator Age(double age)
+			=> new Age(age);
+		public static implicit operator double(Age age)
+			=> age.age;
+	}
+
+}
---
+++ FP_csharp1/ExampleBasedLib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleBasedLib.Datas;
+
+namespace ExampleBasedLib
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			ExampleBasedLib.Datas.Type.main();
+
+		}
+	}
+}
