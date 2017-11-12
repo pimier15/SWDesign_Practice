2ac0d833dfb22fb42dcc073096d5e5b8c8772f89
pimier15 <pimier15@gmail.com>
2017-11-12 16:19:58 +0900

    update

---
+++ functional-csharp-code/Practice/dataType/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.dataType
+{
+	public static class HandlerTest
+	{
+
+	}
+
+
+
+	public static class Maybe2
+	{
+
+
+	}
+
+
+	namespace MaybeType
+	{
+		public struct None
+		{
+			public readonly Nothing
+		}
+
+		public struct Some<A>
+		{
+			public readonly A Value;
+
+			public Some( A val )
+			{
+				if ( val == null ) throw new ArgumentException();
+				Value = val;
+			}
+
+
+		}
+
+
+	}
+
+
+
+
+	class Class1
+	{
+	}
+
+
+
+}
