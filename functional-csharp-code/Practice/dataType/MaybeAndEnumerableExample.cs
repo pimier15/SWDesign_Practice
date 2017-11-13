75b4c6a6e36e5d776a3c8bce33fbebe5cf9c74f0
pimier15 <pimier15@gmail.com>
2017-11-14 07:50:55 +0900

    up

---
+++ functional-csharp-code/Practice/dataType/MaybeAndEnumerableExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.dataType
+{
+	public class MaybeAndEnumerableExample
+	{
+		class Subject
+		{
+			public Maybe<Age> Age { get; set; }
+		}
+
+		// Flattend IEnuemrable<Maybe<Age>> => IEnuermable<Age>
+		IEnumerable<Subject> Population => new[]
+		{
+			new Subject { Age = Age.Of(33)} ,
+			new Subject { } ,
+			new Subject { Age = Age.Of(45)}
+		};
+
+
+		public void main()
+		{
+			// need to calculate avg of age
+
+			var avg = Population.Select( x => x.Age.Value).Average();
+
+
+
+
+		}
+
+
+
+	}
+
+	
+	
+}
